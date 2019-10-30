4d2ee8b2e63982dcb243816cc99bbf8fb28adbce
elvisfb <elvisfb@edu.unisinos.br>
2019-10-30 17:11:53 -0300

    Trocado icone do tkinter e adicionado lista de widgets na janela

--- application.py
+++ application.py
@@ -1,15 +1,11 @@
 # -*- coding: latin-1 -*-
 from tkinter import *
+from tkinter import messagebox
 import subprocess
 from threading import Thread 
-import paho.mqtt.client as mqtt 
 import window as wd
 
-#brokerStartBtn = Button(window)
-#brokerStartBtn.pack()
-
-window = wd.Window()
-window.exec()
+serverState = False
 
 class Broker(Thread):
     def __init__(self):
@@ -17,8 +13,26 @@ class Broker(Thread):
     
     def run(self):
         p = subprocess.Popen("mosquitto -v", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()        
+        out, err = p.communicate()  
 
+def btnBrokerServer():
+    global serverState
+    if not serverState:
+        objBroker.start()
+        serverState = True
+    else:
+        objBroker.stop()
+        serverState = False
+
+objBroker = Broker()
+
+window = wd.Window("dbIcon.ico")
+
+btnBroker = "Start Broker"
+
+window.addBtn(55, btnBroker, btnBrokerServer)
+
+window.exec()
 #objBroker = Broker()
 #objBroker.start()
 
--- window.py
+++ window.py
@@ -3,12 +3,34 @@ from tkinter import *
 class Window:
     def __init__(self):
         self.window = Tk()
+        self.setWindow()
+
+    def __init__(self, icon):
+        self.window = Tk()
+        self.window.iconbitmap(default=icon)
+        self.setWindow()
+
+    def setWindow(self):
+        self.widgetElements = dict()
         self.x_size = self.window.winfo_screenwidth() // 2
         self.y_size = self.window.winfo_screenheight() // 2
 
         windowPosition = str(self.x_size) + "x" + str(self.y_size) + "+" + str(self.x_size//2) + "+" + str(self.y_size//2)
-        print(windowPosition)
         self.window.geometry(windowPosition)
 
     def exec(self):
         self.window.mainloop()
+
+    def getWindow(self):
+        return self.window
+
+    def addBtn(self, pos, txtBtn, callback):
+        btn = Button(self.window, text = txtBtn, command = callback)
+        btn.pack()
+        self.widgetElements[txtBtn] = btn
+
+    def getElement(self, element):
+        if element in self.widgetElements:
+            return self.widgetElements.get(element)
+        else:
+            raise Exception("O elemento " + element + " não foi adicionado!")
